6122ba2e8e9b67cc5a1fcb64b26530204b110a0a
Amit Solanki <amitsolankisocial8@gmail.com>
2026-09-10 08:19:34 +0530

    feat: add roles property to User model for better role management

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use App\Models\Concerns\HasPublicId;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -32,6 +33,7 @@ use Spatie\Permission\Traits\HasRoles;
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @property-read UserPreference|null $preferences
+ * @property-read Collection<int, Role> $roles
  */
 class User extends Authenticatable implements PasskeyUser
 {
